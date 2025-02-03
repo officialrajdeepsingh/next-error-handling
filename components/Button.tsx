f85ac69150740a0a6a9b2d4e6c32ecd3ffdb72f2
Rajdeep Singh <radewala.2@gmail.com>
2025-02-03 15:43:19 +0530

    button components

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,36 @@
+'use client'
+
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+
+//  It is a trigger error and is caught by a global error file. 
+const globalError = async () =>
+   new Promise((_, rej) => {
+   setTimeout(() => rej("Server-Side Error: An error has occurred; something went wrong."), 500);
+});
 
 export function Buttons(){
-    return (
-         <div className="container flex flex-row gap-5 mb-10 justify-between items-center">
 
-          <Link href={"/admin"} className="text-white bg-black rounded-lg  px-4 py-2 text-center inline-flex items-center"> 
+   const [error, setError] = useState(false)
+
+    useEffect(()=>{   
+       if(error){
+         globalError()
+       }
+    },[error])
+
+    return (
+         <div className="container  flex flex-row gap-5 my-10 justify-between items-center">
+
+          <Link href={"/"} className="text-white bg-black rounded-lg  px-4 py-2 text-center inline-flex items-center"> 
+            Home
+           </Link>
+
+           <Link href={"/admin"} className="text-white bg-black rounded-lg  px-4 py-2 text-center inline-flex items-center"> 
             Error 
            </Link>
 
-          <Link href={"/blog"} className="text-white bg-black rounded-lg  px-4 py-2 text-center inline-flex items-center"> 
-            Global Error
-         </Link>
+           <button onClick={() => setError(true)} className="text-white bg-black rounded-lg  px-4 py-2 text-center inline-flex items-center">  Global Error </button>
 
           <Link href={"/admin/dashboard"} className="text-white bg-black rounded-lg px-4 py-2 text-center inline-flex items-center"> 
              Forbidden
